0ba4220d783de8498a107092e564de3adb3e56ac
ICE <ice.zhang@quintet-digital.com>
2022-10-24 09:47:40 +0800

    1.不用设置环境变量，从env中获取DATABASE_URL。

--- migration/src/main.rs
+++ migration/src/main.rs
@@ -1,6 +1,11 @@
 use sea_orm_migration::prelude::*;
 
+use dotenv;
+use std::env::{self, Vars};
 #[async_std::main]
 async fn main() {
+    let _xx = dotenv::dotenv().ok();
+    let url = std::env::var("DATABASE_URL").expect("Environment variable 'DATABASE_URL' not set");
+    println!("DATABASE_URL:{:?}", url);
     cli::run_cli(migration::Migrator).await;
 }
